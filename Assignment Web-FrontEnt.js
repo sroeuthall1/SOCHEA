98e1721307f5ebbe0f3bb4b92df341031d271265
sroeuthall1 <142596996+sroeuthall1@users.noreply.github.com>
2023-08-29 09:18:42 +0700

    Add files via upload

---
+++ Assignment Web-FrontEnt.js
@@ -0,0 +1,16 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+}
+
+// let menu = document.querySelector('.navbar');
+
+// document.querySelector('#menu-icon').onclick = () => {
+//     menu.classList.toggle('active');
+// }
+
+let header = document.querySelector('header');
+window.addEventListener('scroll' , () => {
+    header.classList.toggle('shadow' , window.scrollY > 0);
+});
